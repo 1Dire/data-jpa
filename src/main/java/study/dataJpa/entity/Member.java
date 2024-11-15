816e5b502506323e147c132455ebb199f02ccf57
이상우 <sangwoo@soleo.kr>
2024-11-15 15:48:40 +0900

    jpa 동작확인

---
+++ src/main/java/study/dataJpa/entity/Member.java
@@ -0,0 +1,30 @@
+package study.dataJpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String userName;
+
+    protected Member() {
+    }
+
+    public Member(String userName) {
+        this.userName = userName;
+    }
+
+    public void changeUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
